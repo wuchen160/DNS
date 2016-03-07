778d027100a0888045084f0b96bdbf647450583c
zyqf <qq767026763@gmail.com>
2016-03-07 11:43:13 +0800

    Update uninstall_bind9.10.3.sh

--- bin/uninstall_bind9.10.3.sh
+++ bin/uninstall_bind9.10.3.sh
@@ -1,8 +1,1 @@
-rpm -e bind-9.10.3-3.el6.src.rpm
-rpm -e bind-license-9.10.3-3.el6.noarch.rpm
-rpm -e bind-libs-9.10.3-3.el6.x86_64.rpm
-rpm -e bind-9.10.3-3.el6.x86_64.rpm
-rpm -e bind-chroot-9.10.3-3.el6.x86_64.rpm
-rpm -e bind-debuginfo-9.10.3-3.el6.x86_64.rpm
-rpm -e bind-devel-9.10.3-3.el6.x86_64.rpm
-rpm -e bind-utils-9.10.3-3.el6.x86_64.rpm
+rpm -e bind-9.10.3-3.el6.x86_64
