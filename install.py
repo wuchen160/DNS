7f4088051c0274104018d5f3024e7578a9ae694f
zyqf <qq767026763@gmail.com>
2016-03-09 11:14:48 +0800

    Update and rename move.py to install.py

--- install.py
+++ install.py
@@ -1,12 +1,20 @@
 # -*- coding:utf-8 -*-
 import os
 
+print 'start install bind9...... \n'
+os.system('yum install bind -y')
+
+print 'move some flies ...... '
+os.system('mv /etc/named.conf /etc/named.conf.bak')
 os.system('mv /etc/named.conf /etc/named.conf.bak')
 os.system('mv /etc/named.rfc1912.zones /etc/named.rfc1912.zones.bak')
 os.system('cp -f named.rfc1912.zones/named.rfc1912.zones /etc/named.rfc1912.zones')
 os.system('cp -a -f named/* /var/named')
 os.system('cp -f named.conf /etc/named.conf')
 
+print 'SELINUX is disabling ......'
 os.system('setenforce 0')
 os.system("sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config")
 os.system('service named start')
+
+print'install have done,please enjoy it!'
