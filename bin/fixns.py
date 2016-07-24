6df72b929b520183a4d042718f8de7ef1cd7ef24
zyqf <zyqf@users.noreply.github.com>
2016-07-25 03:09:26 +0800

    Update fixns.py

--- bin/fixns.py
+++ bin/fixns.py
@@ -13,7 +13,9 @@ while True:
 	text = f.read()
 	if 'b.root-servers' not in text:
 		os.system('dig @a.root-servers.net . ns > /usr/local/named/var/named.root')
+		f.close()
 	else:
 		print 'named.root is ok'
 		os.system('service named restart')
+		f.close()
 		sys.exit()
